b452167e618f22a1425ac200a48b70ef75c1ca25
Tosh Corgi <corgitosh@gmail.com>
2021-05-28 19:11:35 +0200

    Added failing tests for user.go

---
+++ user_test.go
@@ -0,0 +1,37 @@
+package godacl
+
+import (
+	"testing"
+)
+
+func TestGetCurrentUser(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestGetUser(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestModifyCurrentUser(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestGetCurrentUserGuilds(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestLeaveGuild(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestCreateDM(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestCreateGroupDM(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
+
+func TestGetUserConnections(t *testing.T) {
+	t.Errorf("Test not implemented")
+}
